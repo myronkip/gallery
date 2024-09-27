e6e266459ecdf9093fcb5640b58357edcaa33636
myronkip <kipngetichkorir6918@gmail.com>
2024-09-27 17:48:55 +0300

    mongodb url

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://myronkip:<Nointernet.1@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://myronkip:Nointernet.1@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    production: 'mongodb+srv://myronkip:Nointernet.1@cluster0.ontd3.mongodb.net/',
+    development: 'mongodb+srv://myronkip:Nointernet.1@cluster0.ontd3.mongodb.net/',
     test: 'mongodb+srv://myronkip:Nointernet.1@cluster0.ontd3.mongodb.net/',
 }
 module.exports = config;
